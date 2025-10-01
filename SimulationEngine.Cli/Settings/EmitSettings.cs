3f0b8f5a5aa1c6fb2f58955571f69861f6ecd731
Sondre Bitubekk <32852496+sonbit@users.noreply.github.com>
2025-10-01 15:49:15 +0200

    Cli: Added description to EmitKind

--- SimulationEngine.Cli/Settings/EmitSettings.cs
+++ SimulationEngine.Cli/Settings/EmitSettings.cs
@@ -1,9 +1,12 @@
 ﻿using SimulationEngine.Cli.Settings.Enums;
 using Spectre.Console.Cli;
+using System.ComponentModel;
 
 namespace SimulationEngine.Cli.Settings;
 
 public sealed class EmitSettings : BaseFindSettings
 {
-    [CommandOption("-k")] public EmitKind EmitKind { get; set; } = EmitKind.Verilog;
+    [CommandOption("-k")]
+    [Description($"{nameof(EmitKind.Verilog)} (Default), {nameof(EmitKind.Testbench)}, {nameof(EmitKind.Top)}, {nameof(EmitKind.Top7Seg)}, {nameof(EmitKind.SevenSeg)}, {nameof(EmitKind.Xdc)}, {nameof(EmitKind.Xdc7Seg)}")]
+    public EmitKind EmitKind { get; set; } = EmitKind.Verilog;
 }
