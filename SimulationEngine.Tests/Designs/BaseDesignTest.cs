a3aebd74aee3c231ff6505f63065b6a1cbab160e
Sondre Bitubekk <32852496+sonbit@users.noreply.github.com>
2025-11-05 05:39:11 +0100

    remove rebel-2 test

--- SimulationEngine.Tests/Designs/BaseDesignTest.cs
+++ SimulationEngine.Tests/Designs/BaseDesignTest.cs
@@ -45,44 +45,6 @@ public abstract class BaseDesignTest(ITestOutputHelper testOutputHelper)
         testOutputHelper.WriteLine($"Elapsed time: {stopWatch.Elapsed}");
     }
 
-    protected void TestSimulatation(Subcircuit subcircuit, List<Subcircuit> subcircuits, string testString, bool skipEvaluation = false)
-    {
-        var simulationSession = SimulationSession.Build(subcircuit);
-
-        var tests = TestStringConverter.GetInputOutputPairs(testString);
-
-        var lineNumber = 1;
-        var stopWatch = Stopwatch.StartNew();
-        var outputString = string.Empty;
-        foreach (var (inputs, expectedOutputs) in tests)
-        {
-            var allEqual = true;
-
-            simulationSession.SetInputs(inputs);
-            var outputs = string.Join(" ", subcircuits.Select(simulationSession.GetOutputs));
-            outputString += outputs + Environment.NewLine;
-            continue;
-            Assert.True(skipEvaluation || outputs.Length == expectedOutputs.Length);
-
-            for (var i = 0; i < outputs.Length; i++)
-            {
-                var equal = outputs[i] == expectedOutputs[i];
-                Assert.True(skipEvaluation || equal, GetEvaluationString(lineNumber, inputs, expectedOutputs, outputs, equal));
-
-                if (skipEvaluation && !equal)
-                    allEqual = false;
-            }
-
-            if (skipEvaluation)
-                testOutputHelper.WriteLine(GetEvaluationString(lineNumber, inputs, expectedOutputs, outputs, allEqual));
-
-            lineNumber++;
-        }
-
-        stopWatch.Stop();
-        testOutputHelper.WriteLine($"Elapsed time: {stopWatch.Elapsed}");
-    }
-
     private static string GetEvaluationString(int lineNumber, string inputs, string expectedOutputs, string outputs, bool equal) =>
         $"{lineNumber}: {inputs} -> {expectedOutputs} {(equal ? "==" : "!=")} {outputs}";
 }
