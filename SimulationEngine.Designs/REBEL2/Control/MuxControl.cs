6c84de3dd186afcc6cee84ffd0f0856d93d9b6b1
Steven Bos <steven@stevenbos.com>
2025-11-23 04:17:52 +0100

    New REBEL-2 Mux from OC

--- SimulationEngine.Designs/REBEL2/Control/MuxControl.cs
+++ SimulationEngine.Designs/REBEL2/Control/MuxControl.cs
@@ -16,6 +16,8 @@ public class MuxControl : Subcircuit
     public Port AluBMux => Outputs[1];
     public Port ProgCtr => Outputs[2];
     public Port AddAMux => Outputs[3];
+    public Port AddBMux => Outputs[4];
+    public Port WrEnable => Outputs[5];
 
     public MuxControl()
     {
@@ -30,36 +32,66 @@ public class MuxControl : Subcircuit
             nameof(AluAMux),
             nameof(AluBMux),
             nameof(ProgCtr),
-            nameof(AddAMux));
+            nameof(AddAMux),
+            nameof(AddBMux),
+            nameof(WrEnable));
 
-        var rdd = this.AddLogicGate("RDD");
-        var _9VZ9VZ0VZ = this.AddLogicGate("9VZ9VZ0VZ");
-        var _4rd = this.AddLogicGate("4RD");
-        var d = this.AddLogicGate("D");
-        var x = this.AddLogicGate("X");
-        var h = this.AddLogicGate("H");
+        var _ZZDDDDXXX = this.AddLogicGate("ZZDDDDXXX");
+        var _RDD = this.AddLogicGate("RDD");
+        var _9VZ = this.AddLogicGate("9VZ");
+        var _4RD = this.AddLogicGate("4RD");
+        var _ZHZ = this.AddLogicGate("ZHZ");
+        var _DZZ = this.AddLogicGate("DZZ");
+        var _PPK = this.AddLogicGate("PPK");
+        var _48Z = this.AddLogicGate("48Z");
 
-        var _3MUX1 = this.AddSubcircuit(new _3MUX1());
-
+        
+       
+       
         this.AddWires([
-            (Op1, rdd.B),
-            (Op0, rdd.A),
+            //inputs to gates
+            (Op1, _RDD.B),
+            (Op0, _RDD.A),
 
-            (Rd1, _9VZ9VZ0VZ.C),
-            (Op1, _9VZ9VZ0VZ.B),
-            (Op0, _9VZ9VZ0VZ.A),
+            (Op1, _9VZ.B),
+            (Op0, _9VZ.A),
+            
+            (Op1, _4RD.B),
+            (Op0, _4RD.A),
 
-            (Op1, _4rd.B),
-            (Op0, _4rd.A),
+            (Op1, _ZHZ.B),
+            (Op0, _ZHZ.A),
 
-            (Rd0, d.A),
-            (Rd0, x.A),
-            (Rd0, h.A),
+             //RD(2)1 is not connected
+            //(Rd1, ???),
+            
+            (Rd0, _DZZ.A),
+            (Rd0, _PPK.A),
+            (Rd0, _ZZDDDDXXX.B),
 
-            (rdd.Q, AluAMux),
-            (_9VZ9VZ0VZ.Q, AluBMux),
-            (_3MUX1.Q, ProgCtr),
-            (h.Q, AddAMux)
+            (Cmp, _48Z.A),  
+            (Cmp, _ZZDDDDXXX.A),  
+            
+            //gate 1st column
+            (_RDD.Q, _DZZ.B),
+            (_RDD.Q, AluAMux),
+            
+            (_9VZ.Q, _PPK.B),
+           
+            (_4RD.Q, _ZZDDDDXXX.C),
+            (_4RD.Q, _48Z.B),
+
+            (_ZHZ.Q, WrEnable),
+
+            //gate 2nd column
+             (_DZZ.Q, AddAMux),
+
+             (_PPK.Q, AluBMux),
+
+             (_ZZDDDDXXX.Q, ProgCtr),
+
+             (_48Z.Q, AddBMux),
+
         ]);
     }
 }
--- SimulationEngine.Designs/REBEL2/REBEL2.cs
+++ SimulationEngine.Designs/REBEL2/REBEL2.cs
@@ -52,6 +52,7 @@ public class REBEL2 : Subcircuit
         var alu_a_mux = this.AddSubcircuit(new _2MUX2());
         var alu_b_mux = this.AddSubcircuit(new _3MUX2());
         var add_a_mux = this.AddSubcircuit(new _2MUX2());
+        var add_b_mux = this.AddSubcircuit(new _3MUX2());
         var alu = this.AddSubcircuit(new ALU2());
         var wr_add = this.AddSubcircuit(new _2TritAdder()); // Ignore Carry
 
@@ -139,6 +140,14 @@ public class REBEL2 : Subcircuit
             (prog_ctr.Pc1, add_a_mux.A1),
             (prog_ctr.Pc0, add_a_mux.A0),
 
+            (cpuControl.Add_B_Mux_Ctrl, add_b_mux.Sel),
+            (instr_reg.Rs01, add_b_mux.C0),
+            (instr_reg.Rs00, add_b_mux.C1),
+            (instr_reg.Rd11, add_b_mux.B0),
+            (instr_reg.Rd10, add_b_mux.B1),
+            (instr_reg.Rd01, add_b_mux.A0),
+            (instr_reg.Rd00, add_b_mux.A1),
+
             (cpuControl.Alu_Func2, alu.Func2),
             (cpuControl.Alu_Func1, alu.Func1),
             (cpuControl.Alu_Func0, alu.Func0),
@@ -148,10 +157,10 @@ public class REBEL2 : Subcircuit
             (alu_b_mux.Q0, alu.A0),
 
             (add_a_mux.Q1, wr_add.B1),
-            (add_a_mux.Q1, wr_add.B0),
-            (instr_reg.Rs01, wr_add.A1),
-            (instr_reg.Rs00, wr_add.A0),
-
+            (add_a_mux.Q0, wr_add.B0),
+            (add_b_mux.Q1, wr_add.A1),
+            (add_b_mux.Q0, wr_add.A0),
+           
             //(alu.Cout, ), 
             (alu.Q1, reg.WrData1),
             (alu.Q0, reg.WrData0),
