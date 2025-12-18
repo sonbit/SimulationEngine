57a732970912b93d90ba849777997d6d2bf1598d
Sondre Bitubekk <32852496+sonbit@users.noreply.github.com>
2025-12-18 07:19:35 +0100

    Removed some control tests, REBEL-2 is already verified

--- SimulationEngine.Tests/Designs/REBEL2/CPUControlTests.cs
+++ SimulationEngine.Tests/Designs/REBEL2/CPUControlTests.cs
@@ -6,14 +6,5 @@ namespace SimulationEngine.Tests.Designs.REBEL2;
 public class CPUControlTests(ITestOutputHelper testOutputHelper) : BaseDesignTest(testOutputHelper)
 {
     [Fact]
-    public void AluControlFlow_Validate() => TestSimulatation(new AluControlFlow(), true);
-
-    [Fact]
-    public void AluControlWithShift_Validate() => TestSimulatation(new AluControlWithShift(), true);
-
-    [Fact]
-    public void CPUControl_Validate() => TestSimulatation(new CPUControl(), true);
-
-    [Fact]
     public void MuxControl_Validate() => TestSimulatation(new MuxControl());
 }
